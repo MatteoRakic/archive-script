914011718d14715959c0ddc0ad8356a62fd5bdec
MatteoRakic <matteo.rakic@mydata.co.za>
2025-07-16 11:07:50 +0200

    feat: working version completed pre demo

--- archive-script/src/consts.py
+++ archive-script/src/consts.py
@@ -1,9 +1,9 @@
 S3_BUCKET = "artemis-int-ingestion-files-af-south-1-992382549029"
-TARGET_PREFIX = "workstage=landing-archive"
-SOURCE_PREFIX = "workstage=landing"
+TARGET_PREFIX = "source=archive/source=evo/origin=sacrra/workstage=landing"
+SOURCE_PREFIX = "source=evo/origin=sacrra/workstage=landing"
 MAX_FILE_AGE_IN_DAYS = 10
 
 TEST_BUCKET = "neil-992382549029"
-TEST_TARGET_PREFIX = "matt_archived"
+TEST_TARGET_PREFIX = "source=archive/source=evo/origin=sacrra/matt_archived"
 TEST_SOURCE_PREFIX = "matt_old"
-TEST_MAX_FILE_AGE_IN_MINS = 10
+TEST_MAX_FILE_AGE_IN_MINS = 1
--- archive-script/src/main.py
+++ archive-script/src/main.py
@@ -1,39 +1,52 @@
+import time
 from datetime import datetime, timedelta
 
 import boto3
 import polars as pl
-from logger import logger
 
-cnt = 0
+from consts import (
+    TEST_BUCKET,
+    TEST_MAX_FILE_AGE_IN_MINS,
+    TEST_SOURCE_PREFIX,
+    TEST_TARGET_PREFIX,
+)
+from logger import logger
 
 s3_client = boto3.client("s3")
 
-
-srn_to_load = "decrypted-sorted-srn"
-
-from consts import *
 
 def list_all_keys(bucket):
     keys = []
     paginator = s3_client.get_paginator("list_objects_v2")
     # pages = paginator.paginate(Bucket=bucket,Prefix=prefix,PaginationConfig={'MaxItems': 10})
-    pages = paginator.paginate(Bucket=bucket, Prefix=SOURCE_PREFIX)
+    pages = paginator.paginate(Bucket=bucket, Prefix=TEST_SOURCE_PREFIX)
 
     for page in pages:
         if "Contents" in page:
             for obj in page["Contents"]:
-                if obj["Key"] != SOURCE_PREFIX + "/":
+                if obj["Key"] != TEST_SOURCE_PREFIX + "/":
+                    key = obj["Key"]
+                    filename = key.replace(TEST_SOURCE_PREFIX + "/", "")
+                    filename = filename.replace(".", "_")
+                    srn = filename.split("_")[0]
                     keys.append(
-                        (obj["Key"], obj["LastModified"].strftime("%Y-%m-%dT%H:%M:%SZ"))
+                        (key, srn, obj["LastModified"].strftime("%Y-%m-%dT%H:%M:%SZ"))
                     )
 
     return keys
 
 
-# Fetch all object keys
-all_keys = list_all_keys(S3_BUCKET)
+# Start timer
+timer = time.time()
 
-df = pl.DataFrame(schema={"key_filename": pl.Utf8, "Date": pl.Utf8}, data=all_keys)
+# Fetch all object keys
+all_keys = list_all_keys(TEST_BUCKET)
+
+df = pl.DataFrame(
+    schema={"key_filename": pl.Utf8, "srn": pl.Utf8, "Date": pl.Utf8},
+    data=all_keys,
+    orient="row",
+)
 
 # Convert Date column from string to datetime
 df = df.with_columns(pl.col("Date").str.strptime(pl.Datetime, "%Y-%m-%dT%H:%M:%SZ"))
@@ -43,34 +56,44 @@ df_archive = df.filter(
     pl.col("Date") < datetime.now() - timedelta(minutes=TEST_MAX_FILE_AGE_IN_MINS)
 )
 
-# Test for files older than 10 Days
-df_archive = df.filter(
-    pl.col("Date") < datetime.now() - timedelta(days=MAX_FILE_AGE_IN_DAYS)
-)
+# # Test for files older than 10 Days
+# df_archive = df.filter(
+#     pl.col("Date") < datetime.now() - timedelta(days=MAX_FILE_AGE_IN_DAYS)
+# )
 
 if len(df_archive) == 0:
     logger.info("No files to archive")
 else:
-    logger.info(f"Found {len(df_archive)} files to archive:")
+    logger.info("Found %d files to archive:", len(df_archive))
 
 # Move files from source to target prefix
 for row in df_archive.iter_rows(named=True):
     source_key = row["key_filename"]
-    key_filename = source_key.replace(f"{SOURCE_PREFIX}/", "")
-    target_key = f"{TARGET_PREFIX}/{key_filename}"
-
+    print(source_key)
+    srn = row["srn"]
+    effective_year_month = row["Date"].strftime("%Y-%m")
+    key_filename = source_key.replace(f"{TEST_SOURCE_PREFIX}/", "")
+    target_key = f"{TEST_TARGET_PREFIX}/supplier-reference-number={srn}/effective-year-month={effective_year_month}/{key_filename}"
     try:
         # Copy object to target location
-        s3_client.copy_object(
-            Bucket=S3_BUCKET,
-            CopySource={"Bucket": S3_BUCKET, "Key": source_key},
+        response = s3_client.copy_object(
+            Bucket=TEST_BUCKET,
+            CopySource={"Bucket": TEST_BUCKET, "Key": source_key},
             Key=target_key,
         )
-        logger.info(f"Successfully copied {source_key} to {target_key}")
 
-        # Delete original object (completing the "move")
-        s3_client.delete_object(Bucket=S3_BUCKET, Key=source_key)
-        logger.info(f"Successfully deleted {source_key}")
+        s3_status = response.get("ResponseMetadata", {}).get("HTTPStatusCode", 0)
+        if s3_status == 200:
+            logger.info("Successfully copied %s to %s", {source_key}, {target_key})
+            # Delete original object (completing the "move")
+            s3_client.delete_object(Bucket=TEST_BUCKET, Key=source_key)
+            logger.info("Successfully deleted %s", {source_key})
+        else:
+            logger.error("Failed to copy %s to %s", {source_key}, {target_key})
 
     except Exception as e:
-        logger.error(f"Error moving file {source_key}: {str(e)}")
+        logger.error("Error moving file %s: %s", {source_key}, {str(e)})
+
+# End timer and log elapsed time
+elapsed_time = time.time() - timer
+logger.info("Script completed in %d seconds", elapsed_time)
